0d6d843bf5af35eea1da143eeff6c4bdaf5c7c79
agrup <agustinrodriguez206@gmail.com>
2018-04-06 03:06:06 -0300

    agregada entrada al tp2

--- TP2/ej10/subir_imagen.php
+++ TP2/ej10/subir_imagen.php
@@ -7,7 +7,7 @@ $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $target_thumbs_file = $target_thumbs . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
+
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false) {
