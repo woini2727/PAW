fa6249f31dc2827826765384b3c9a6e9048aeb93
agrup <agustinrodriguez206@gmail.com>
2018-04-23 15:44:15 -0300

    arrancado ej4

---
+++ TP2-2/ej3/core/Config.php
@@ -0,0 +1,13 @@
+ <?php 
+
+class Config {
+
+	private $configFile = __DIR__ . "/../config.php.sample";
+	public $db;
+	public function __construct(){
+		$this->db = require $this->configFile;
+	}
+
+}
+
+  ?>
