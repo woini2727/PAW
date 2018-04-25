ee44739d3742860cad52963b0bc850a9bf52ad51
agrup <agustinrodriguez206@gmail.com>
2018-04-25 03:32:09 -0300

    sql agregados

--- TP2-2/turnos/ej2/views/show.turno.php
+++ TP2-2/turnos/ej2/views/show.turno.php
@@ -5,9 +5,12 @@
 	<title>Su turno</title>
 </head>
 <body><h2>Su numero de Turno: </h2>
+	<h2>
+		
 	<?php if($ultimoTurno): ?>
        <?= $ultimoTurno ?>		
 	<?php endif;?>
+	</h2>
 	<a href="../index.php">Volver</a>
 </body>
 </html>
