358cd992c7ef840e63ede9256245bd73694c0a71
woini2727 <salvador.woini@gmail.com>
2018-05-03 20:30:02 -0300

    tp12ej8

---
+++ TP3/ej12/ej8tp2-1/imprimir_turno_get.php
@@ -0,0 +1,25 @@
+
+<?php  
+
+
+
+	$titulo=$_GET["titulo"];
+	$nombre=$_GET["nombre"];
+	$email=$_GET["email"];
+	$minutos=$_GET["minutos"];
+	$telefono=$_GET["telefono"];
+	$edad=$_GET["edad"];
+	$talle=$_GET["talle"];
+	$altura=$_GET["altura"];
+	$fecha_nac=$_GET["fecha_nacimiento"];
+	$pelo=$_GET["color_pelo"];
+	$turno=$_GET["horario"];
+	echo "Los datos de mi turno son: <br>".$titulo.":</h3> <h1>".$nombre.".</h1> <br> Su email es: ".$email."<br> Tel: ".$telefono." Edad: ".$edad." <br> Talle:".$talle."  Altura: ".$altura." Fecha de nacimiento: ".$fecha_nac." <br> Color de pelo".$pelo." <br> Su turno fue guardado para: ".$turno.":".$minutos; 
+
+
+
+
+
+//diferencia entre get y post http://localhost/paw/imprimirTurno.php?titulo=aa&nombre=Salvador&email=salvador.woini%40gmail.com&telefono=s23&edad=2&talle=21&altura=1%2C50&fecha_nacimiento=2018-03-21&color_pelo=dsd&horario=8%3A00
+	?>
+
---
+++ TP3/ej12/ej9tp2-1/imprimir_turno_post.php
@@ -0,0 +1,15 @@
+<?php  
+	$titulo=$_POST["titulo"];
+	$nombre=$_POST["nombre"];
+	$email=$_POST["email"];
+	$minutos=$_POST["minutos"];
+	$telefono=$_POST["telefono"];
+	$edad=$_POST["edad"];
+	$talle=$_POST["talle"];
+	$altura=$_POST["altura"];
+	$fecha_nac=$_POST["fecha_nacimiento"];
+	$pelo=$_POST["color_pelo"];
+	$turno=$_POST["horario"];
+	echo "Los datos de mi turno son: <br>".$titulo.":</h3> <h1>".$nombre.".</h1> <br> Su email es: ".$email."<br> Tel: ".$telefono." Edad: ".$edad." <br> Talle:".$talle."  Altura: ".$altura." Fecha de nacimiento: ".$fecha_nac." <br> Color de pelo".$pelo." <br> Su turno fue guardado para: ".$turno.":".$minutos; 
+
+	?>
