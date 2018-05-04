ba5c6d505b9f4b654f5d53b5b761a39de830ec95
woini2727 <salvador.woini@gmail.com>
2018-05-04 03:10:28 -0300

    tp3css

--- TP3/ej12/ej9tp2-1/imprimir_turno_post.php
+++ TP3/ej12/ej9tp2-1/imprimir_turno_post.php
@@ -1,3 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
 <?php  
 	$titulo=$_POST["titulo"];
 	$nombre=$_POST["nombre"];
@@ -10,6 +17,9 @@
 	$fecha_nac=$_POST["fecha_nacimiento"];
 	$pelo=$_POST["color_pelo"];
 	$turno=$_POST["horario"];
-	echo "Los datos de mi turno son: <br>".$titulo.":</h3> <h1>".$nombre.".</h1> <br> Su email es: ".$email."<br> Tel: ".$telefono." Edad: ".$edad." <br> Talle:".$talle."  Altura: ".$altura." Fecha de nacimiento: ".$fecha_nac." <br> Color de pelo".$pelo." <br> Su turno fue guardado para: ".$turno.":".$minutos; 
+	echo "Los datos de mi turno son:<h3> <br>".$titulo.":</h3> <h1>".$nombre.".</h1> <br> Su email es: ".$email."<br> Tel: ".$telefono." Edad: ".$edad." <br> Talle:".$talle."  Altura: ".$altura." Fecha de nacimiento: ".$fecha_nac." <br> Color de pelo".$pelo." <br> Su turno fue guardado para: ".$turno.":".$minutos; 
 
 	?>
+</body>
+</html>
+
