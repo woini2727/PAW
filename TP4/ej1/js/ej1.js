e170a0d1f174c3af3ffba713c1ed2b9b693ae5fb
woini2727 <salvador.woini@gmail.com>
2018-05-16 19:50:37 -0300

    ej1

--- TP4/ej1/js/ej1.js
+++ TP4/ej1/js/ej1.js
@@ -4,34 +4,53 @@ var console = console || {},
 
 
 		MemoTest={
-			 tamaño: 4,
+			 tamaño: 3,
+			 imagenes: ['patiVacio', 'pati1', "pati2"],
 			iniciar:function nplayers(contenedor){
 				
 				"use strict";
 				var div = document.getElementById(contenedor);
 				this.armarGrilla(div);
+				this.armarMemo(div);
 				},
 
 			armarGrilla:function(div){
 				var egrilla = document.createElement("h3");
-				tgrilla = document.createTextNode(this.tamaño);
+				tgrilla = document.createTextNode("tamaño del juego: "+ this.tamaño);
 				 egrilla.appendChild(tgrilla);
 				 div.appendChild(egrilla);},
 
 			armarMemo:function(div){ 
-				if(this.tamaño==3){
-					var img = document.createElement("img");
-					div.appendChild(egrilla);
-				}
-				else if (this.tamaño==4) {
-					
-				}else if (this.tamaño==5) {
+				var tamañoActual=this.tamaño;
+				var table = document.createElement("TABLE");
+				//table.setAttribute("id", "myTable");
+				var tbody=document.createElement("TBODY");
+				//tbody.setAttribute("id", "tbody");
+				
+					for (var i = 0; i < tamañoActual; i++) {
+							var tr=document.createElement("TR");
+							//tr.setAttribute("id", "tr");
+							
+							for (var j = 0; j < tamañoActual; j++) {
+								var td=document.createElement("TD");
+								var img = document.createElement("img");
+								img.src="img/blanco.png";
+								
+								td.appendChild(img);
+								//document.getElementById("tr").appendChild(td);
 
-				}
-				//div.addEventListener("click", function (event) {
-          		//var event = event || e;
-          		//event.target.setAttribute("class", "Ahorcado_tecla Ahorcado_teclaUsada");          document.getElementById("Ahorcado_palabraBuscada").innerHTML = Ahorcado.palabraBuscada();
-        		//})
+								tr.appendChild(td);
+								img.addEventListener("click", function (event) {
+						        var event = event || e;
+						        event.target.src="img/argentina.png";
+						        })
+
+							}
+							table.appendChild(tr);													
+						}
+						div.appendChild(table);
+
+
 			},
 		
 };
