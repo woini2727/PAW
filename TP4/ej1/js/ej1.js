1354291b43ce740b9e921773dc5f09aaec2c91b4
woini2727 <salvador.woini@gmail.com>
2018-05-17 00:43:30 -0300

    ej1

--- TP4/ej1/js/ej1.js
+++ TP4/ej1/js/ej1.js
@@ -28,27 +28,41 @@ var console = console || {},
 				//table.setAttribute("id", "myTable");
 				var tbody=document.createElement("TBODY");
 				//tbody.setAttribute("id", "tbody");
-				
+				var n=0;
 					for (var i = 0; i < tamañoActual; i++) {
+
 							var tr=document.createElement("TR");
 							//tr.setAttribute("id", "tr");
 							
 							for (var j = 0; j < tamañoActual; j++) {
 								var td=document.createElement("TD");
 								var img = document.createElement("img");
+								
+								img.setAttribute("id",n)
 
 								//var random=Math.floor((Math.random() * this.imagenes.size) + 0);
-								var imagSelec=this.imagenes.pop();
-								
-								img.src=imagSelec;
-								
-								
-								
+								//var imagSelec=this.imagenes.pop();
+								n++;
+								img.src="img/blanco.png";
+
 								td.appendChild(img);
 								//document.getElementById("tr").appendChild(td);
 
 								tr.appendChild(td);
-								
+								img.addEventListener("click", function (event) {
+						          var event = event || e;
+						          //antes de dar vuelta me fijo si hay mas de 2 levantadas
+						          cantFlip=MemoTest.verFlip();
+						          console.log(cantFlip);
+						          if (cantFlip<2) {
+						          //doy vuelta la carta
+						          event.target.src=MemoTest.imagenes[parseInt(event.target.id)];
+						          event.target.value="flip";
+						          }
+						         
+						          //busco otra prendida y la comparo
+
+						        })
 
 							}
 							table.appendChild(tr);													
@@ -67,8 +81,19 @@ var console = console || {},
 				    }
 				    return a;
 				},
+			verFlip:function(){
+				var cantFlip=0;
+				for (var i = 0; i < this.imagenes.length; i++) {
+					img=document.getElementById(i);
+					if (img.value=="flip") {
+						cantFlip++;
+					}
+				}	
+				return cantFlip;
+			},
+			
 		
 };
 
- 
+
 
