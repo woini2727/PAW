b7776ebc5a7c297a733389ebfdebeb5cd66ab974
agrup <agustinrodriguez206@gmail.com>
2018-05-16 22:58:23 -0300

    js creado

---
+++ TP4/ej2/index.js
@@ -0,0 +1,4 @@
+         document.css.getElementById("myColor").setAttribute('Value','green');
+    $color=document.css.getElementById("myColor").Value;
+    $css = document.getElementsByTagName('html') ;   
+    $css.style.cssText="'--main-cc"+$color;
