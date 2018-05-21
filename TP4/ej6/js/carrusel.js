2f13d016c07f410c34bcfb3fc730e6106fe8cfcf
woini2727 <salvador.woini@gmail.com>
2018-05-21 05:39:42 -0300

    ej6 empezado

---
+++ TP4/ej6/js/carrusel.js
@@ -0,0 +1,46 @@
+var console = console || {},
+  document = document || {},
+
+
+  	Carrusel={
+  		imagenes:["img/marca xbo.jpg","img/marca r.jpg","img/marcas ea.jpg"],
+  		n:0,
+  		iniciar:function(div){
+  			var div = document.getElementById(div);
+  			var img=document.createElement("img");
+  			img.src=this.imagenes[0];
+  			
+  			img.setAttribute("id","imag");
+  			img.setAttribute("width","800px");
+  			img.setAttribute("height","300px");
+  			
+  			div.appendChild(img);
+  		},
+  		anttimg:function(){
+  			
+  		
+  			var img = document.getElementById("imag");
+  			if(this.n==0){
+  				this.n=this.imagenes.length;
+  				img.src=this.imagenes[this.n-1];
+  			}else if (this.n==this.imagenes.length){
+  				this.n=this.n-2;
+  				img.src=this.imagenes[this.n];
+  			}else{
+  				this.n=this.n-1;
+  				img.src=this.imagenes[this.n];
+  			}
+  			  			
+  		},
+  		nextimg:function(){
+  			console.log(this.n);
+  			var img = document.getElementById("imag");
+  				this.n=this.n+1;
+  				if (this.n==this.imagenes.length){
+  				this.n=0;}
+  				img.src=this.imagenes[this.n];
+  				
+  		
+
+  		}
+  	};
