7c1535951e1fc1461be3d8e47d44a8ab5ea1babd
agrup <agustinrodriguez206@gmail.com>
2018-05-21 17:22:08 -0300

    reprod auto

--- TP4/ej7/js/index.js
+++ TP4/ej7/js/index.js
@@ -1,33 +1,35 @@
-var console = console || {},
+  var console = console || {},
   document = document || {},
+
 
 video ={
     videoIndex : 1,
-    
+
+
     plusVideo:function(n) {
-      this.showVideo( this.slideIndex += n);
+      this.showVideo( this.videoIndex += n);
     },
 
     currentVideo: function(n)  {
-       this.showVideo( this.slideIndex = n);
+       this.showVideo( this.videoIndex = n);
     },
         
     
     showVideo:function(n) {
       var i;
-      var video = document.getElementsByClassName("mySlides");
-      var dots = document.getElementsByClassName("demo");
+      var video = document.getElementsByClassName("videoContainer");
       var captionText = document.getElementById("caption");
       if (n > video.length) {this.videoIndex = 1}
       if (n < 1) {this.videoIndex = video.length}
       for (i = 0; i < video.length; i++) {
-          video[i].style.display = "none";
+          video[i].style.visibility = "none";
+
       }
-      for (i = 0; i < dots.length; i++) {
-          dots[i].className = dots[i].className.replace(" active", "");
-      }
-      video[this.videoIndex-1].style.display = "block";
-      dots[this.videoIndex-1].className += " active";
-      captionText.innerHTML = dots[this.videoIndex-1].alt;
+
+      video[this.videoIndex-1].style.visibility = "visible";
+
+      video[this.videoIndex-1].autoplay;
+   
+      
     }
 }
