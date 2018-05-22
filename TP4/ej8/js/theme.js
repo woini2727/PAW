8550aed969f951141b201dca85bf822f42734290
woini2727 <salvador.woini@gmail.com>
2018-05-22 04:47:09 -0300

    ej8 terminado v1

--- TP4/ej8/js/theme.js
+++ TP4/ej8/js/theme.js
@@ -3,11 +3,24 @@ $(document).ready(function(){
    $("#tema").on({
    		click: function(){
    			var color = $("body").css('background-color');
-   			console.log(color);
+   			
    			if(color=="rgb(255, 238, 230)"){
    			$("body").css("background-color", "azure");}
    		else{
    			$("body").css("background-color", "rgb(255, 238, 230)");
    		}}
    		});
+
+  //cambio la vista
+   $("#vista").on({
+   		click: function(){
+   			var display = $(".fd2").css('display');
+   			console.log(display);
+   			if(display=="flex"){
+   				$(".fd2").css("display","block");
+   			}else{
+   				$(".fd2").css("display","flex");
+   			}
+   		}
+   })
 });
