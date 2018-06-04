80aa706bfaeadfbb0d51ff365006c1e8e02d4059
agrup <agustinrodriguez206@gmail.com>
2018-06-04 02:45:03 -0300

    buscador en maps

--- TP5/ej10/js/index.js
+++ TP5/ej10/js/index.js
@@ -23,11 +23,11 @@ function iniciar(){
         });
         var input = /** @type {!HTMLInputElement} */(
             document.getElementById('pac-input'));
-        var types = document.getElementById('type-selector');
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+      
+        //map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 /*
 */
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
+
         var autocomplete = new google.maps.places.Autocomplete(input);
         autocomplete.bindTo('bounds', map);
 
