7421ecb58ffab62caed66d63d1d52a869bbf581e
agrup <agustinrodriguez206@gmail.com>
2018-07-01 18:50:51 -0300

    estilo agregado

--- ClickerGame/Game/src/socket.js
+++ ClickerGame/Game/src/socket.js
@@ -13,18 +13,18 @@ module.exports = function(io){
 			let ub = Admin.addGames(dir[4],dir[5],dir[6],s1);
 
 			socket.on('clickemit',(clickData)=>{
-			console.log(clickData);
-				//io.sockets.emit('clickAtack',clickData);
+			//console.log(clickData);
+			//io.sockets.emit('clickAtack',clickData);
 
 			socket.broadcast.emit('clickAtack',clickData);
 			});
 				socket.on('clickplayer',(clickData)=>{
-				console.log(clickData);
+				//console.log(clickData);
 				//io.sockets.emit('clickAtack',clickData);
-				console.log(Admin.getMap().get(socket.id),socket.id);
+				//console.log(Admin.getMap().get(socket.id),socket.id);
 				//socket.broadcast.emit('clickAtack',clickData);
-				let oponent = Admin.getMap().get(socket.id);
-				socket.broadcast.to(oponent).emit('clickAtack',clickData);
+				//let oponent = Admin.getMap().get(socket.id);
+				socket.broadcast.to(Admin.getMap().get(socket.id)).emit('clickAtack',clickData);
 			});
 
 	});
